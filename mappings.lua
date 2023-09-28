ee74279adb9d3de714a2063000c2456c5e4c7775
MvdS <m.schans@robeco.nl>
2023-09-28 22:27:45 +0200

    formatting

--- mappings.lua
+++ mappings.lua
@@ -1,33 +1,34 @@
 local M = {}
 
 M.dap = {
-	plugin = true,
-	n = {
-		["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" },
-	},
+  plugin = true,
+  n = {
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" },
+  },
 }
 
 M.dap_python = {
-	plugin = true,
-	n = {
-		["<leader>dpr"] = {
-			function()
-				require("dap-python").test_method()
-			end,
-		},
-	},
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require("dap-python").test_method()
+      end,
+    },
+  },
 }
 
 M.copilot = {
-	i = {
-		["<C-l>"] = {
-			function()
-				vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
-			end,
-			"Copilot Accept",
-			{ replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
-		},
-	},
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+      end,
+      "Copilot Accept",
+      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
+    },
+  },
+}
 }
 
 return M
