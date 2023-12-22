8000f3483dc4eedef5d480cd45251c05f20a1855
mvds314 <postaanmartin@gmail.com>
2023-12-22 13:39:21 +0100

    Fix timeout problems with formatting

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -48,7 +48,7 @@ local cspellconfig = {
 local opts = {
   sources = {
     -- Python
-    null_ls.builtins.formatting.black.with { extra_args = { "--line-length", 99 }, timeout = -1 },
+    null_ls.builtins.formatting.black.with { extra_args = { "--line-length", 99 } },
     null_ls.builtins.diagnostics.pylint,
     -- null_ls.builtins.diagnostics.flake8,
     -- null_ls.builtins.diagnostics.mypy,
@@ -74,16 +74,18 @@ local opts = {
     -- null_ls.builtins.diagnostics.textidote,
   },
   on_attach = function(client, bufnr)
+    -- run manually with :lua print(vim.lsp.buf.format())
     if client.supports_method "textDocument/formatting" then
       vim.api.nvim_clear_autocmds {
         group = augroup,
         buffer = bufnr,
       }
+      -- https://github.com/nvimtools/none-ls.nvim/wiki/Formatting-on-save
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format { bufnr = bufnr }
+          vim.lsp.buf.format { async = false, timeout_ms = 10000 }
         end,
       })
     end
