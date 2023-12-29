13b75280b6db76ed4fc56238121c89a91ae665c8
mvds314 <postaanmartin@gmail.com>
2023-12-29 13:07:09 +0100

    Autoreload buffer when changed on disk

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
 vim.opt.relativenumber = true
+
+-- Autoreload on buffer change
+-- https://stackoverflow.com/questions/62100785/auto-reload-file-and-in-neovim-and-auto-reload-nerbtree
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
