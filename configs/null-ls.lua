e822f8e08933b646a3ec3edb22755f354ac8c9fb
MvdS <m.schans@robeco.nl>
2024-03-27 10:48:50 +0100

    none-ls-extras

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -50,11 +50,12 @@ local opts = {
     -- Python
     -- null_ls.builtins.formatting.black.with { extra_args = { "--line-length", 99 } },
     -- null_ls.builtins.formatting.ruff.with { extra_args = { "--line-length", 99 } },
+    -- require("none-ls.formatting.ruff"),--.with { extra_args = { "--line-length", 99 } }, -- for some reason this does not work
     -- null_ls.builtins.diagnostics.pylint,
     -- null_ls.builtins.diagnostics.flake8,
     -- null_ls.builtins.diagnostics.mypy,
     -- null_ls.builtins.diagnostics.ruff.with {
-      -- extra_args = { "--ignore", "E501", "--ignore", "E741", "--ignore", "E731", "--ignore", "E402" },
+    -- extra_args = { "--ignore", "E501", "--ignore", "E741", "--ignore", "E731", "--ignore", "E402" },
     -- },
     -- Formatters: Lua, json, yaml, md
     null_ls.builtins.formatting.stylua,
