8087e66f08b9c2596a4973a136213b8bf78d8b02
MvdS <m.schans@robeco.nl>
2024-04-15 10:11:28 +0200

    reformat

--- plugins.lua
+++ plugins.lua
@@ -107,7 +107,7 @@ local plugins = {
       "nvim-lua/plenary.nvim", -- required
       "nvim-telescope/telescope.nvim", -- optional
       "sindrets/diffview.nvim", -- optional
-      -- "ibhagwan/fzf-lua",              -- optional
+      -- "ibhagwan/fzf-lua", -- optional
     },
     config = true,
   },
