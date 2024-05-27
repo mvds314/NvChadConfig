69be5269ef36bbf71856a747c3db33a92238c87a
MvdS <m.schans@robeco.nl>
2024-05-27 13:15:20 +0200

    init autoreload

--- init.lua
+++ init.lua
@@ -37,3 +37,14 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+
+----------------------------------------- CUSTOM -----------------------------------------------------
+
+-- Autoreload on buffer change
+-- https://stackoverflow.com/questions/62100785/auto-reload-file-and-in-neovim-and-auto-reload-nerbtree
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
