bde42d2bc446b8ac08ac69887e0c01c70818f926
MvdS <m.schans@robeco.nl>
2024-07-04 13:59:50 +0200

    lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -118,6 +118,25 @@ return {
     },
     config = true,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+    },
+  },
   --{
   --  "rcarriga/nvim-dap-ui",
   --  dependencies = { "mfussenegger/nvim-dap", "LiadOz/nvim-dap-repl-highlights", "nvim-neotest/nvim-nio" },
