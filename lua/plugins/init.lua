6031c444de6e5523d57593f1b489facb439b74e7
MvdS <m.schans@robeco.nl>
2024-07-04 13:59:40 +0200

    reformat

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -111,9 +111,9 @@ return {
     "NeogitOrg/neogit",
     lazy = false,
     dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
+      "nvim-lua/plenary.nvim", -- required
       "nvim-telescope/telescope.nvim", -- optional
-      "sindrets/diffview.nvim",        -- optional
+      "sindrets/diffview.nvim", -- optional
       -- "ibhagwan/fzf-lua", -- optional
     },
     config = true,
