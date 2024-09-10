29fa9b6a1718131505d76775a50b57928de02050
Schans <rob6027@robeco.nl>
2024-09-10 12:47:54 +0200

    Add Mason ensure installed to chadrc.lua

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,55 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "onedark",
 }
+
+M.mason.pkgs = {
+  -- "pyright",
+  -- "python-lsp-server",
+  "jedi-language-server",
+  -- "black",
+  -- "ruff",
+  "ruff-lsp",
+  -- "flake8",
+  -- "pylint",
+  "debugpy",
+  "lua-language-server",
+  "stylua",
+  "texlab",
+  -- "ltex-ls",
+  "latexindent",
+  "codespell",
+  "harper-ls",
+  -- "vale", -- vale.init not found
+  "proselint",
+  "json-lsp",
+  -- "fixjson",
+  --"jsonlint",
+  "marksman",
+  -- "mdformat",
+  "prettierd",
+  -- "luasnip",
+  "vim-language-server",
+  "vint",
+  "yaml-language-server",
+  -- "yamlfix",
+  -- "taplo", -- TOML LSP: https://taplo.tamasfe.dev/
+  -- "dprint", -- formatting for several languages including TOML
+  "dockerfile-language-server",
+  --"azure-pipelines-language-server",
+  "hadolint",
+  "bash-language-server",
+  "shfmt",
+}
+
+-- hl_override = {
+-- 	Comment = { italic = true },
+-- 	["@comment"] = { italic = true },
+-- },
 
 return M
