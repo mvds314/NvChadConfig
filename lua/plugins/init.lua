ebc53493b6cf5a1508eff093747a269e6168e74a
mvds314 <postaanmartin@gmail.com>
2024-09-14 13:32:02 +0200

    Add oil.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -124,6 +124,15 @@ return {
       },
     },
   },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
   -------------------------------- REPLS --------------------------------
   {
     "Vigemus/iron.nvim",
