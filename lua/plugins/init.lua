9146ec8ffaec23fa9ca63437ff84bcf5c3134b1b
mvds314 <postaanmartin@gmail.com>
2024-09-14 20:25:26 +0200

    Add substitute.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -87,6 +87,21 @@ return {
   },
   { "psliwka/vim-smoothie", lazy = false },
   {
+    "gbprod/substitute.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    config = function()
+      require("substitute").setup {}
+      vim.keymap.set("n", "s", require("substitute").operator, { noremap = true })
+      vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
+      vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
+      vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
+    end,
+  },
+  {
     "ThePrimeagen/harpoon",
     cmd = "Harpoon",
   },
