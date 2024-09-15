422684ddc04b9c83edc6a6d333811097fe32e4c4
mvds314 <postaanmartin@gmail.com>
2024-09-15 09:54:24 +0200

    Add flash.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -88,6 +88,20 @@ return {
   { "psliwka/vim-smoothie", lazy = false },
   -- TODO: test this plugin
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+  -- TODO: test this plugin
+  {
     "gbprod/substitute.nvim",
     opts = {
       -- your configuration comes here
