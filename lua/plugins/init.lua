787d4e63612d8dd4982bac9e06f174d909cbfc4a
Schans <rob6027@robeco.nl>
2024-09-18 14:33:34 +0200

    Comment to fix later

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -70,6 +70,8 @@ return {
       if is_windows then
         conf.extensions.whaler = {
           directories = { "C:\\Users\\ROB6027\\Repos" },
+          -- TODO: try to do this with an environment variable
+          -- directories = { "$USERPROFILE\\Repos" },
           oneoff_directories = { "$LOCALAPPDATA\\nvim" },
           file_explorer = "nvimtree",
           auto_file_explorer = false, -- Whether to automatically open file explorer. By default is `true`
