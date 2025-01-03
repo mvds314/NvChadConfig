7a7d02b2bfd2e8939448804e7c88f1a766adc693
Schans <rob6027@robeco.nl>
2025-01-03 16:40:04 +0100

    Add texsuite

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -53,9 +53,11 @@ return {
       "nvim-telescope/telescope-fzy-native.nvim",
       "nvim-telescope/telescope-symbols.nvim",
       "SalOrak/whaler",
+      -- "kiyoon/telescope-insert-path.nvim", TODO: try this plugin
+      "cagve/telescope-texsuite",
     },
     -- TODO: Test these plugins:
-    -- https://github.com/cagve/telescope-texsuite
+    -- https://github.com/cagve/telescope-texsuite -> for LaTeX, test it further
     -- https://github.com/paopaol/telescope-git-diffs.nvim
     -- https://github.com/xiyaowong/telescope-emoji.nvim
     -- neoclip
@@ -67,7 +69,7 @@ return {
       local is_windows = vim.fn.has "win64" == 1 or vim.fn.has "win32" == 1 or vim.fn.has "win16" == 1
       local is_linux = vim.fn.has "unix" == 1
       -- And edit it as described here: https://nvchad.com/docs/config/plugins
-      conf.extensions_list = { "themes", "terms", "fzy_native", "whaler", "symbols" }
+      conf.extensions_list = { "themes", "terms", "fzy_native", "whaler", "symbols", "texsuite" }
       conf.extensions.fzy_native = { override_generic_sorter = true, override_file_sorter = true }
       -- TODO: this doesn't seem to work properly as bot .git and .gitignore are ignored -> test this in Linux
       -- conf.defaults.file_ignore_patterns = { "^.git/*" }
