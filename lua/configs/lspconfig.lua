c83b4a96751e3f44a85c0e7277591e7d9d58de33
mvds314 <postaanmartin@gmail.com>
2025-02-23 19:55:52 +0100

    Refactor python on_attach

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -27,31 +27,32 @@ end
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 ------------------------------------- Python LSPs -------------------------------------------
+local function python_on_attach(_, bufnr)
+  -- https://docs.astral.sh/ruff/integrations/#language-server-protocol-official
+  -- https://docs.astral.sh/ruff/integrations/#vim-neovim
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+  -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  -- vim.keymap.set('n', '<space>wl', function()
+  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end, bufopts)
+  -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+end
 
 -- lspconfig.pyright.setup {
---   on_attach = function(_, bufnr)
---     -- https://docs.astral.sh/ruff/integrations/#language-server-protocol-official
---     -- https://docs.astral.sh/ruff/integrations/#vim-neovim
---     -- Enable completion triggered by <c-x><c-o>
---     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
---
---     -- Mappings.
---     -- See `:help vim.lsp.*` for documentation on any of the below functions
---     local bufopts = { noremap = true, silent = true, buffer = bufnr }
---     -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
---     vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
---     vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
---     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
---     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
---     -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
---     -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
---     -- vim.keymap.set('n', '<space>wl', function()
---     --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---     -- end, bufopts)
---     -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
---     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
---     vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
---   end,
+--   on_attach = python_on_attach,
 --   capabilities = nvlsp.capabilities,
 --   filetypes = { "python" },
 --   -- https://github.com/microsoft/pyright/blob/main/docs/settings.md
@@ -68,37 +69,28 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 --     },
 --   },
 -- }
---
 
 -- Configure Pylyzer
-lspconfig.pylyzer.setup {
-  on_attach = function(_, bufnr)
-    -- https://docs.astral.sh/ruff/integrations/#language-server-protocol-official
-    -- https://docs.astral.sh/ruff/integrations/#vim-neovim
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    -- vim.keymap.set('n', '<space>wl', function()
-    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, bufopts)
-    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-  end,
-  -- Add any specific configuration options for Pylyzer here
-  capabilities = nvlsp.capabilities,
-  filetypes = { "python" },
-}
+-- lspconfig.pylyzer.setup {
+--   on_attach = python_on_attach,
+--   -- Add any specific configuration options for Pylyzer here
+--   capabilities = nvlsp.capabilities,
+--   filetypes = { "python" },
+--   settings = {
+--     python = {
+--       checkOnType = false,
+--       diagnostics = false,
+--       inlayHints = false,
+--       smartCompletion = true,
+--     },
+--   },
+--   single_file_support = true,
+--   command = {
+--     "pylyzer",
+--     "--server",
+--     "--disable diagnostics codeLens hover inlayHint semanticTokens signatureHelp documentLink ",
+--   },
+-- }
 
 -- lspconfig.jedi_language_server.setup {
 --   on_attach = function(_, bufnr)
