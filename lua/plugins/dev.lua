2214922c9d8cfb93ed2bd3c23aac2e51f6e37e58
mvds314 <postaanmartin@gmail.com>
2025-05-10 18:07:30 +0200

    Fix

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,11 +1,13 @@
+local is_windows = vim.loop.os_uname().version:match "Windows"
+local dir
+if is_windows then
+  dir = os.getenv "USERPROFILE" .. "/Repos/myplugin.nvim"
+else
+  dir = os.getenv "HOME" .. "/Repos/myplugin.nvim"
+end
 return {
   {
-    dir = function()
-      local is_windows = vim.loop.os_uname().version:match "Windows"
-      local retval = is_windows and (os.getenv "USERPROFILE" .. "/Repos/myplugin.nvim")
-        or (os.getenv "HOME" .. "/Repos/myplugin.nvim")
-      return retval
-    end,
+    dir = dir,
     config = function()
       print "Running config"
     end,
