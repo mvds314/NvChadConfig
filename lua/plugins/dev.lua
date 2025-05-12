50f7a4caa7097552c63075acc56850ec82f60bc1
Schans <rob6027@robeco.nl>
2025-05-12 08:32:35 +0200

    Fix if plugin does not exists

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -5,12 +5,21 @@ if is_windows then
 else
   dir = os.getenv "HOME" .. "/Repos/myplugin.nvim"
 end
-return {
-  {
-    dir = dir,
-    config = function()
-      print "Running config"
-    end,
-    lazy = false,
-  },
-}
+local function dir_exists(path)
+  local stat = vim.loop.fs_stat(path)
+  return stat and stat.type == "directory"
+end
+if dir_exists(dir) then
+  return {
+    {
+      dir = dir,
+      enable = false,
+      config = function()
+        print "Running config"
+      end,
+      lazy = false,
+    },
+  }
+else
+  return {}
+end
