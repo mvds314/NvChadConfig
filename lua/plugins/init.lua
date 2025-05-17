4f35785b6df77797b718d2787e6ce842d40ba927
mvds314 <postaanmartin@gmail.com>
2025-05-17 17:14:40 +0200

    Add lazydev for better lua lsp features

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -54,6 +54,17 @@ return {
       }
     end,
   },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
   ----------------------------- Navigation -----------------------------
   {
     "nvim-telescope/telescope.nvim",
