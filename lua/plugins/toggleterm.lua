91a85a95bee65785341fef1ceef218cfad07d3c5
mvds314 <mvds314@users.noreply.github.com>
2025-05-29 16:18:06 +0200

    Save file before running it

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,6 +2,8 @@ local ipy_term = nil
 
 local run_python_file_in_ipython_terminal = function()
   local file = vim.api.nvim_buf_get_name(0)
+  -- Save the file before running it
+  vim.cmd "write"
   -- local tt = require "toggleterm"
   local Terminal = require("toggleterm.terminal").Terminal
   if file == "" then
