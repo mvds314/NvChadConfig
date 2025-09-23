ae04ba3af22134881ee2d4f2952ccfd567824c34
MvdS <postaanmartin@gmail.com>
2025-09-23 22:31:20 +0200

    Add development of togglepy.nvim

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,25 +1,29 @@
+local M = {}
+
 local is_windows = vim.loop.os_uname().version:match "Windows"
+
+local function dir_exists(path)
+  local stat = vim.loop.fs_stat(path)
+  return stat and stat.type == "directory"
+end
+
 local dir
 if is_windows then
   dir = os.getenv "USERPROFILE" .. "/Repos/myplugin.nvim"
 else
   dir = os.getenv "HOME" .. "/Repos/myplugin.nvim"
 end
-local function dir_exists(path)
-  local stat = vim.loop.fs_stat(path)
-  return stat and stat.type == "directory"
+if dir_exists(dir) then
+  table.insert(M, { dir = dir, lazy = false, enable = false, config = function() end })
+end
+
+if is_windows then
+  dir = os.getenv "USERPROFILE" .. "/Repos/togglepy.nvim"
+else
+  dir = os.getenv "HOME" .. "/Repos/togglepy.nvim"
 end
 if dir_exists(dir) then
-  return {
-    {
-      dir = dir,
-      enable = false,
-      config = function()
-        -- print "Running config"
-      end,
-      lazy = false,
-    },
-  }
-else
-  return {}
+  table.insert(M, { dir = dir, lazy = false, enable = false, config = function() end })
 end
+
+return M
