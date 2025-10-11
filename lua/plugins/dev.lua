e0b5065d2ff3da834bfa358e98c07d5fac891267
MvdS <postaanmartin@gmail.com>
2025-10-11 21:56:45 +0200

    opts and keymaps to config

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -33,44 +33,16 @@ if dir_exists(dir) then
       "mfussenegger/nvim-dap", -- DAP core
       "akinsho/toggleterm.nvim",
     },
-    -- config = function()
-    --   require("togglepy").setup { host = "localhost", port = 9000 }
-    --   -- require "togglepy.repl"
-    -- end,
-    --  TODO: define opts and keymaps here
-    -- opts = {
-    --   git = {
-    --     enable = true,
-    --   },
-    --   view = {
-    --     side = "left",
-    --   },
-    -- },
-    -- keys = {
-    --   {
-    --     "<leader>ga",
-    --     mode = "n",
-    --     function()
-    --       local api = require "nvim-tree.api"
-    --       local node = api.tree.get_node_under_cursor()
-    --       local gs = node.git_status.file
-    --       -- If the current node is a directory get children status
-    --       if gs == nil then
-    --         gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1])
-    --           or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
-    --       end
-    --       -- If the file is untracked, unstaged or partially staged, we stage it
-    --       if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
-    --         vim.cmd("silent !git add " .. node.absolute_path)
-    --       -- If the file is staged, we unstage
-    --       elseif gs == "M " or gs == "A " then
-    --         vim.cmd("silent !git restore --staged " .. node.absolute_path)
-    --       end
-    --       api.tree.reload()
-    --     end,
-    --     desc = "Stage/unstage file",
-    --   },
-    -- },
+    opts = {
+      host = "localhost",
+      port = 9000,
+    },
+    keys = {
+      { "<C-w>h", "<C-\\><C-n><C-w>h", mode = "t", noremap = true, desc = "Go to left window" },
+      { "<C-w>j", "<C-\\><C-n><C-w>j", mode = "t", noremap = true, desc = "Go to lower window" },
+      { "<C-w>k", "<C-\\><C-n><C-w>k", mode = "t", noremap = true, desc = "Go to upper window" },
+      { "<C-w>l", "<C-\\><C-n><C-w>l", mode = "t", noremap = true, desc = "Go to right window" },
+    },
   })
 end
 -- Auto-generate helptags when saving a doc file
