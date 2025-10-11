29d8071061e29be51278ac81199fef88cf824cfe
MvdS <postaanmartin@gmail.com>
2025-10-11 20:47:54 +0200

    Opts and keymaps

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -28,7 +28,6 @@ if dir_exists(dir) then
     -- lazy = false,
     ft = "python",
     enable = true,
-    -- TODO: fix this, and generate the docs automatically
     build = ":helptags " .. dir .. "/doc",
     dependencies = {
       "mfussenegger/nvim-dap", -- DAP core
@@ -38,6 +37,40 @@ if dir_exists(dir) then
     --   require("togglepy").setup { host = "localhost", port = 9000 }
     --   -- require "togglepy.repl"
     -- end,
+    --  TODO: define opts and keymaps here
+    -- opts = {
+    --   git = {
+    --     enable = true,
+    --   },
+    --   view = {
+    --     side = "left",
+    --   },
+    -- },
+    -- keys = {
+    --   {
+    --     "<leader>ga",
+    --     mode = "n",
+    --     function()
+    --       local api = require "nvim-tree.api"
+    --       local node = api.tree.get_node_under_cursor()
+    --       local gs = node.git_status.file
+    --       -- If the current node is a directory get children status
+    --       if gs == nil then
+    --         gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1])
+    --           or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
+    --       end
+    --       -- If the file is untracked, unstaged or partially staged, we stage it
+    --       if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+    --         vim.cmd("silent !git add " .. node.absolute_path)
+    --       -- If the file is staged, we unstage
+    --       elseif gs == "M " or gs == "A " then
+    --         vim.cmd("silent !git restore --staged " .. node.absolute_path)
+    --       end
+    --       api.tree.reload()
+    --     end,
+    --     desc = "Stage/unstage file",
+    --   },
+    -- },
   })
 end
 -- Auto-generate helptags when saving a doc file
