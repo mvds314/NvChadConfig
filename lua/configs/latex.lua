f52f22c0ba0573c9017b63513dc4432bd4e12a57
MvdS <postaanmartin@gmail.com>
2025-10-23 14:01:32 +0200

    Fix

--- lua/configs/latex.lua
+++ lua/configs/latex.lua
@@ -1,17 +1,22 @@
 -- Some logic to autmatically download the spell files if not present
-local spell_dir = vim.fn.stdpath "config" .. "/spell"
+local sep = package.config:sub(1, 1)
+local spell_dir = vim.fn.stdpath "config" .. sep .. "spell"
 local files = {
-  "nl.utf-8.spl",
-  "nl.utf-8.sug",
+  -- TODO: download these files automatically if they are missing
+  -- Download some dic file, and generate them with :mkspell ~/.config/nvim/spell/nl nl.dic
+  -- "nl.utf-8.spl",
+  -- "nl.utf-8.sug",
 }
 
-local base_url = "http://ftp.vim.org/vim/runtime/spell/"
+-- TODO: this url is not working
+local base_url = "https://ftp.nluug.nl/pub/vim/runtime/spell/"
 
 -- Function to download a file using curl
 local function download_file(filename)
   local url = base_url .. filename
-  local output_path = spell_dir .. "/" .. filename
+  local output_path = vim.fn.fnamemodify(spell_dir, ":p") .. filename
   local cmd = string.format("curl -fLo '%s' --create-dirs '%s'", output_path, url)
+  print(cmd)
   os.execute(cmd)
 end
 
@@ -31,7 +36,7 @@ vim.api.nvim_create_autocmd("FileType", {
       for _, file in ipairs(files) do
         local path = spell_dir .. "/" .. file
         if vim.fn.filereadable(path) == 0 then
-          print("Downloading missing spell file: " .. file)
+          vim.notify("Downloading missing spell file: " .. file)
           download_file(file)
         end
       end
