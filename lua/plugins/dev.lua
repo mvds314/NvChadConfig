f9f41ab2cc0022e078deb25196f8f94990cd88dd
mvds314 <mvds314@users.noreply.github.com>
2025-11-02 15:31:05 +0100

    make local dev of togglepy.nvim optional

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -22,37 +22,60 @@ if is_windows then
 else
   dir = os.getenv "HOME" .. "/Repos/togglepy.nvim"
 end
-if dir_exists(dir) then
-  local search_paths = {}
-  if is_windows then
-    -- Add all WinPython environments located at C:\Software\WPy64* folders
-    local wpy_handle = io.popen 'dir /b /ad "C:\\Software\\WPy64*" 2>nul'
-    if wpy_handle then
-      for folder in wpy_handle:lines() do
-        local wpy_python_handle = io.popen('dir /b /ad "C:\\Software\\' .. folder .. '\\python*" 2>nul')
-        if wpy_python_handle then
-          for subfolder in wpy_python_handle:lines() do
-            table.insert(search_paths, "C:\\Software\\" .. folder .. "\\" .. subfolder)
-          end
-          wpy_python_handle:close()
+local search_paths = {}
+if is_windows then
+  -- Add all WinPython environments located at C:\Software\WPy64* folders
+  local wpy_handle = io.popen 'dir /b /ad "C:\\Software\\WPy64*" 2>nul'
+  if wpy_handle then
+    for folder in wpy_handle:lines() do
+      local wpy_python_handle = io.popen('dir /b /ad "C:\\Software\\' .. folder .. '\\python*" 2>nul')
+      if wpy_python_handle then
+        for subfolder in wpy_python_handle:lines() do
+          table.insert(search_paths, "C:\\Software\\" .. folder .. "\\" .. subfolder)
         end
-        local envs_dir = "C:\\Software\\" .. folder .. "\\envs"
-        -- Add all subfolders of the environments folder
-        local envs_handle = io.popen('dir /b /ad "' .. envs_dir .. '" 2>nul')
-        if envs_handle then
-          for subenv in envs_handle:lines() do
-            table.insert(search_paths, envs_dir .. "\\" .. subenv .. "\\Scripts")
-          end
-          envs_handle:close()
-        end
+        wpy_python_handle:close()
       end
-      wpy_handle:close()
+      local envs_dir = "C:\\Software\\" .. folder .. "\\envs"
+      -- Add all subfolders of the environments folder
+      local envs_handle = io.popen('dir /b /ad "' .. envs_dir .. '" 2>nul')
+      if envs_handle then
+        for subenv in envs_handle:lines() do
+          table.insert(search_paths, envs_dir .. "\\" .. subenv .. "\\Scripts")
+        end
+        envs_handle:close()
+      end
     end
-  else
-    search_paths = { "~/mypython/bin" }
+    wpy_handle:close()
   end
+else
+  search_paths = { "~/mypython/bin" }
+end
+
+local local_dev = false
+if dir_exists(dir) and local_dev then
   table.insert(M, {
     dir = dir,
+    -- lazy = false,
+    ft = "python",
+    enable = true,
+    build = ":helptags " .. dir .. "/doc",
+    dependencies = {
+      "mfussenegger/nvim-dap", -- DAP core
+      "akinsho/toggleterm.nvim",
+    },
+    opts = {
+      ipdab = {
+        host = "localhost",
+        port = 9000,
+      },
+      repl = { search_paths = search_paths, add_miniconda = true, add_system_path = true },
+      keys = {},
+    },
+    keys = {},
+  })
+else
+  table.insert(M, {
+    "mvds314/togglepy.nvim",
     -- lazy = false,
     ft = "python",
     enable = true,
