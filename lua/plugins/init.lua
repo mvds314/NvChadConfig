79eb2d8308885ccffa00294d324b3d7c95a14d2c
MvdS <postaanmartin@gmail.com>
2025-11-04 11:39:51 +0100

    Drop duplicate lspconfig

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -549,12 +549,12 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
