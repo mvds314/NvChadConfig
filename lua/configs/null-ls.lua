6b44de3d396c205be1c1f1508a4561e5144917d9
MvdS <postaanmartin@gmail.com>
2026-05-22 20:15:22 +0200

    Neovim 12 compatibility issue

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -48,7 +48,7 @@ local opts = {
   },
   on_attach = function(client, bufnr)
     -- run manually with :lua print(vim.lsp.buf.format())
-    if client.supports_method "textDocument/formatting" then
+    if client:supports_method "textDocument/formatting" then
       vim.api.nvim_clear_autocmds {
         group = augroup,
         buffer = bufnr,
